115ee773f0edcd29a2870a4a16222c5ad705439c
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2025-02-10 09:11:33 +0100

    Created a separate options file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,5 @@
--- Sets the default leader key for Neovim
--- This config uses <space> as default
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- Sets default options specific to Neovim
+require("config/options")
 
 -- Downloads and initializes the lazy.nvim plugin manager
 require("lazy-bootstrap")
---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,4 @@
+-- Sets the default leader key for Neovim
+-- This config uses <space> as default
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
