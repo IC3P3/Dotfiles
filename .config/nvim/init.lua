a4377becf0ab0888e6cf26b68334c43971c94238
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2025-02-10 11:43:18 +0100

    Added keymaps file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,6 @@
--- Sets default options specific to Neovim
+-- Sets default options and settings specific to Neovim
 require("config/options")
+require("config/keymaps")
 
 -- Downloads and initializes the lazy.nvim plugin manager
 require("lazy-bootstrap")
