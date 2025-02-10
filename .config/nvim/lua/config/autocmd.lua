b9bd6f73e20170cefa59dddb3c3a8957152dc67b
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2025-02-10 14:45:40 +0100

    Added highlights to yanked area

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -1,2 +1,10 @@
+-- Higlights the area that got yanked
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
 
 -- vim: ts=2 sts=2 sw=2 et
