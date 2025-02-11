c5aa7b9426d6f3cf2eeb5b2606ccf50de2605777
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2025-02-11 09:02:41 +0100

    Added Neovide settings

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -7,4 +7,26 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+-- Load Neovide settings when the UI is loaded
+vim.api.nvim_create_autocmd("UIEnter", {
+	group = vim.api.nvim_create_augroup("SetGUISettings", { clear = true }),
+	callback = function()
+		if vim.g.neovide then
+			-- Use given Fonts if available
+			vim.o.guifont = "Lilex Nerd Font,FiraCode Nerd Font,JetBrainsMono Nerd Font:h16"
+
+			-- Makes animation time shorter
+			vim.g.neovide_scroll_animation_length = 0.15
+			vim.g.neovide_scroll_animation_far_lines = 0.5
+			vim.g.neovide_cursor_animation_length = 0.07
+			vim.g.neovide_cursor_trail_size = 0.3
+
+			-- Other settings
+			vim.g.neovide_hide_mouse_when_typing = true
+			vim.g.neovide_refresh_rate = 120
+			vim.g.neovide_detach_on_quit = "promt"
+		end
+	end,
+})
+
 -- vim: ts=2 sts=2 sw=2 et
--- README.md
+++ README.md
@@ -2,4 +2,15 @@
 
 ## Available Config Files
 
-- [Neovim](.config/nvim/README.md)
+- [Neovim + Neovide](.config/nvim/README.md)
+
+## Dependencies
+
+### Software
+
+- [Neovide](https://neovide.dev/)
+- [Neovim](https://neovim.io/)
+
+### Fonts
+
+- [Lilex Nerd Font,FiraCode Nerd Font and/or JetBrainsMono Nerd Font](https://www.nerdfonts.com/)
