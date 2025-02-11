848d3fd2995d4fc0958afea1585a7f2f8b117b7c
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2025-02-11 08:24:36 +0100

    Tried sorting options logically

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
+-- Forces Neovim to use use en_US.UTF8
+vim.cmd("silent! language en_US.UTF8")
+
 -- Sets the default leader key for Neovim
 -- This config uses <space> as default
 vim.g.mapleader = " "
@@ -7,24 +10,15 @@ vim.g.maplocalleader = " "
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- Syncs the clipboard between the OS and Neovim
--- Lazy loads it at "UiEnter" to not impact the startup-time
-vim.schedule(function()
-	vim.opt.clipboard = "unnamedplus"
-end)
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
 
 -- Enables break indent
 vim.opt.breakindent = true
 
--- Save undo history to file
-vim.opt.undofile = true
-
--- Makes searches case-insensitive unless \C or a capital letter is used
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = "yes"
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 4
+vim.opt.sidescrolloff = 12
 
 -- Sets how neovim displays certain whitespaces
 vim.opt.list = true
@@ -34,24 +28,27 @@ vim.opt.listchars = {
 	nbsp = "␣",
 }
 
--- Preview subsitionen live
-vim.opt.inccommand = "split"
-
--- Highlights the line the cursor is in
-vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 4
-vim.opt.sidescrolloff = 12
-
--- Forces Neovim to use use en_US.UTF8
-vim.cmd("silent! language en_US.UTF8")
-
 -- Set behaviour of tabs
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+-- Makes searches case-insensitive unless \C or a capital letter is used
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Preview subsitionen live
+vim.opt.inccommand = "split"
+
+-- Syncs the clipboard between the OS and Neovim
+-- Lazy loads it at "UiEnter" to not impact the startup-time
+vim.schedule(function()
+	vim.opt.clipboard = "unnamedplus"
+end)
+
+-- Save undo history to file
+vim.opt.undofile = true
 
 -- Decrease update time
 vim.opt.updatetime = 250
