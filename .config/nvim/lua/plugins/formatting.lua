4878f50090e716648b2da8bd904e944523f0c1e0
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2025-02-13 22:03:40 +0100

    Added autopairs.nvim

---
+++ .config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,10 @@
+return {
+	-- Automatically comlpetes pairs like brackets or quotation marks
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+}
+
+-- vim: ts=2 sts=2 sw=2 et
