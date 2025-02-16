046facc1bf7d24b3fa9ef653a86e2e2fb9bbdbd9
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2025-02-16 21:26:16 +0100

    Added linting support

---
+++ .config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,27 @@
+return {
+	-- "An asynchronous linter plugin for Neovim complementary to the built-in
+	-- Language Server Protocol support."
+	{
+		"mfussenegger/nvim-lint",
+		commit = "6e9dd545a1af204c4022a8fcd99727ea41ffdcc8",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local lint = require("lint")
+			lint.linters_by_ft = {}
+			-- Create autocommand which carries out the actual linting
+			-- on the specified events.
+			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+				group = lint_augroup,
+				callback = function()
+					-- Only run the linter in buffers that you can modify in order to
+					-- avoid superfluous noise, notably within the handy LSP pop-ups that
+					-- describe the hovered symbol using Markdown.
+					if vim.opt_local.modifiable:get() then
+						lint.try_lint(nil, { ignore_errors = true })
+					end
+				end,
+			})
+		end,
+	},
+}
