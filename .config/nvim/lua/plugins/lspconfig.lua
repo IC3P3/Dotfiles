ae43f3d591ec5ca6bc19f0711de0a7db4c23e655
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2025-02-16 21:15:02 +0100

    Added commets to lspconfig

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,4 +1,6 @@
 return {
+	-- "lazydev.nvim is a plugin that properly configures LuaLS for editing
+	-- your Neovim config by lazily updating your workspace libraries."
 	{
 		"folke/lazydev.nvim",
 		tag = "v1.9.0",
@@ -9,15 +11,19 @@ return {
 			},
 		},
 	},
+	-- "Meta type definitions for the Lua platform Luvit."
 	{
 		"Bilal2453/luvit-meta",
 		commit = "1df30b60b1b4aecfebc785aa98943db6c6989716",
 		lazy = true,
 	},
+	-- "Portable package manager for Neovim that runs everywhere Neovim runs."
 	-- TODO: Remove mason later with automatic LSP installer
 	{ "williamboman/mason.nvim", opts = {} },
 	"williamboman/mason-lspconfig.nvim",
 	"WhoIsSethDaniel/mason-tool-installer.nvim",
+	-- "nvim-lspconfig is a "data only" repo, providing basic, default Nvim
+	-- LSP client configurations for various LSP servers."
 	{
 		"neovim/nvim-lspconfig",
 		tag = "v1.6.0",
