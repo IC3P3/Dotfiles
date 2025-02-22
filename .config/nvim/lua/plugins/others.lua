52eaed08928c6c3c5799a051ab1d31e25fe8d333
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2025-02-22 23:36:31 +0100

    Added Todo-Comments

--- .config/nvim/lua/plugins/others.lua
+++ .config/nvim/lua/plugins/others.lua
@@ -1,3 +1,16 @@
-return {}
+return {
+	-- Highlight todo, notes, etc in comments
+	{
+		"folke/todo-comments.nvim",
+		tag = "v1.4.0",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+		config = function()
+			-- Search all TODO comments with Telescope
+			vim.keymap.set("n", "<leader>st", ":TodoTelescope<CR>", { desc = "{TODO-Comment} [S]earch [T]odo" })
+		end,
+	},
+}
 
 -- vim: ts=2 sts=2 sw=2 et
