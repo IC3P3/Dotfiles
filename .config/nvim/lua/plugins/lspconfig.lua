d4f46d8e964510bfed5c8f5d396787928abf90af
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2025-04-19 17:56:45 +0200

    Removed mason

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -26,11 +26,6 @@ return {
 				"hrsh7th/cmp-nvim-lsp",
 				tag = "v0.0.2",
 			},
-			-- "Portable package manager for Neovim that runs everywhere Neovim runs."
-			-- TODO: Remove mason later with automatic LSP installer
-			{ "williamboman/mason.nvim", opts = {} },
-			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
 		},
 		config = function()
 			vim.api.nvim_create_autocmd("LspAttach", {
@@ -79,18 +74,6 @@ return {
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-			local servers = {
-				lua_ls = {
-					settings = {
-						Lua = {
-							completion = {
-								callSnippet = "Replace",
-							},
-						},
-					},
-				},
-				hyprls = {},
-			}
 			vim.diagnostic.config({
 				severity_sort = true,
 				float = { border = "rounded", source = "if_many" },
@@ -129,24 +112,6 @@ return {
 						root_dir = vim.fn.getcwd(),
 					})
 				end,
-			})
-
-			require("mason").setup()
-			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, {
-				"stylua",
-			})
-
-			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-			require("mason-lspconfig").setup({
-				handlers = {
-					function(server_name)
-						local server = servers[server_name] or {}
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-						require("lspconfig")[server_name].setup(server)
-					end,
-				},
 			})
 		end,
 	},
